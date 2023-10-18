dd237784080acdc914bd56d7de7b2da5fd024e3d
Rizky Aprinanda <rizkyaprinanda@gmail.com>
2023-10-18 16:45:34 +0700

    mencoba menggabungkan branch kategori

---
+++ app/src/main/java/com/example/macrogroup6/Category/Sayuran/SayuranCardItem.kt
@@ -0,0 +1,13 @@
+package com.example.macrogroup6.Category.Sayuran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.macrogroup6.R
+
+//class SayuranCardItem : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.sayuran_card_item)
+//    }
+//}
+data class SayuranCardItem(val namaSayuran: String, val gambarResId: Int)
