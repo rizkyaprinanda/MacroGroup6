50685c42e6f731bd2558accae041c5ea981dc15c
Kristo Samosir <kristoisthebest@gmail.com>
2023-10-18 11:29:52 +0900

    membuat fitur informasi seputar stunting

---
+++ app/src/main/java/com/example/macrogroup6/ArtikelStunting.kt
@@ -0,0 +1,11 @@
+package com.example.macrogroup6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArtikelStunting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artikel_stunting)
+    }
+}
