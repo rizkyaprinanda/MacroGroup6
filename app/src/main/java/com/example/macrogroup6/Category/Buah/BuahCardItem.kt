6f95f4ab63293128ddeb78ec7f6bcc0a5de0397c
davv78 <112405097+davv78@users.noreply.github.com>
2023-10-18 15:25:29 +0700

    penambahan halaman kategori buah

--- app/src/main/java/com/example/macrogroup6/Category/Buah/BuahCardItem.kt
+++ app/src/main/java/com/example/macrogroup6/Category/Buah/BuahCardItem.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.macrogroup6.R
 
-class BuahCardItem : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_buah_card_item)
-    }
-}
+//class BuahCardItem : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_buah_card_item)
+//    }
+//}
+
+data class BuahCardItem(val namaSayuran: String, val gambarResId: Int)
--- app/src/main/java/com/example/macrogroup6/Category/Buah/BuahPage.kt
+++ app/src/main/java/com/example/macrogroup6/Category/Buah/BuahPage.kt
@@ -8,5 +8,8 @@ class BuahPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buah_page)
+
+
+
     }
 }
